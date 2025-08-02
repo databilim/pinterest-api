ee98a133b86f4a704a262290cc5bd9640fb78161
Databilim Company <celalettin@erbulut.com>
2025-08-02 12:22:40 +0300

    Update mysql.js

--- db/mysql.js
+++ db/mysql.js
@@ -7,7 +7,7 @@ var db  = mysql.createPool({
   connectionLimit : 10,
   host            : '173.212.203.208',
   user            : 'admin_icerik',
-  password        : 'Cx9544Cx',
+  password        : '*****',
   database        : 'admin_icerik',
   charset         : "utf8mb4",
 });
